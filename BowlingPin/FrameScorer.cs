4a05332e16754f63ca47bbd0e3c99757304ab64a
RichFisher <rich.fishernc@gmail.com>
2019-04-06 20:50:47 -0500

    added manual entry

--- BowlingPin/FrameScorer.cs
+++ BowlingPin/FrameScorer.cs
@@ -15,32 +15,41 @@ namespace BowlingPin
             return new Random().Next(0, remainingPins + 1);
         }
 
-        public void EnterScores(int score)
+        public int EnterScores(string score)
         {
+            return Int32.Parse(score);
         }
 
-        public void ScoreBowl()
+        public void ScoreBowl(bool manual)
         {
             while (CurrentFrame < 10)
             {
-                Bowl();
+                Bowl(manual);
                 StrikeAdder();
                 SpareAdder();
                 CurrentFrame += 1;
-                RemainingPins = 10;
-                FinalFramer();
+                RemainingPins = 10;      
             }
-            ScoreDisplayer();
+            FinalFramer();
+            FinalScoreDisplayer();
         }
 
-        public void Bowl()
+        public void Bowl(bool manual)
         {
-            var bowl1 = GenerateBowl(RemainingPins);
+            if (manual)
+            {
+                Console.WriteLine($"Enter your first bowl score for frame {CurrentFrame + 1}");
+            }
+            var bowl1 = manual ? OptionChooser() : GenerateBowl(RemainingPins);
             RemainingPins -= bowl1;
             var frame = new FrameScore() { Bowl1 = bowl1 };
             if (RemainingPins > 0)
             {
-                var bowl2 = GenerateBowl(RemainingPins);
+                if (manual)
+                {
+                    Console.WriteLine($"Enter your second bowl score for {CurrentFrame + 1}");
+                }
+                var bowl2 = manual ? OptionChooser() : GenerateBowl(RemainingPins);
                 frame.Bowl2 = bowl2;
                 RemainingPins -= bowl2;
                 if (RemainingPins == 0)
@@ -81,26 +90,23 @@ namespace BowlingPin
 
         public void FinalFramer()
         {
-            if (CurrentFrame == 10)
+            if (FrameList[9].Spare)
             {
-                if (FrameList[9].Spare)
+                FrameList[9].TotalScore = 20 - GenerateBowl(RemainingPins);
+            }
+            if (FrameList[9].Strike)
+            {
+                var finalRolls = GenerateBowl(RemainingPins);
+                RemainingPins -= finalRolls;
+                if (RemainingPins > 0)
                 {
-                    FrameList[9].TotalScore = 20 - GenerateBowl(RemainingPins);
+                    finalRolls += GenerateBowl(RemainingPins);
                 }
-                if (FrameList[9].Strike)
+                else
                 {
-                    var finalRolls = GenerateBowl(RemainingPins);
-                    RemainingPins -= finalRolls;
-                    if (RemainingPins > 0)
-                    {
-                        finalRolls += GenerateBowl(RemainingPins);
-                    }
-                    else
-                    {
-                        finalRolls += GenerateBowl(10);
-                    }
-                    FrameList[9].TotalScore = 10 + finalRolls;
+                    finalRolls += GenerateBowl(10);
                 }
+                FrameList[9].TotalScore = 10 + finalRolls;
             }
         }
 
@@ -114,7 +120,34 @@ namespace BowlingPin
             return total;
         }
 
+        public int OptionChooser()
+        {
+            var input = Console.ReadLine();
+            if (input == "s" || input == "S")
+            {
+                ScoreDisplayer();
+                return EnterScores(Console.ReadLine());
+            } else
+            {
+                return EnterScores(input);
+            }
+        }
+
         public void ScoreDisplayer()
+        {
+            var frameCounter = 0;
+            Console.WriteLine("Your current frame scores:");
+            foreach (FrameScore frame in FrameList)
+            {
+                frameCounter++;
+                Console.WriteLine($"Frame {frameCounter}: {frame.TotalScore}--{frame.Bowl1}--" +
+                    $"{(frame.Strike || frame.Spare ? (frame.Strike ? "X" : "/") : $"{frame.Bowl2}")}");
+            }
+            Console.WriteLine($"Total  score: {Totalizer()}");
+            Console.WriteLine("Please enter your next score.");
+        }
+
+        public void FinalScoreDisplayer()
         {
             var frameCounter = 0;
             foreach (FrameScore frame in FrameList)
--- BowlingPin/Program.cs
+++ BowlingPin/Program.cs
@@ -7,7 +7,19 @@ namespace BowlingPin
         static void Main(string[] args)
         {
             var newGame = new FrameScorer();
-            newGame.ScoreBowl();
+            Console.WriteLine("Do you want to manually enter your bowling scores or have them generated automatically?");
+            Console.WriteLine("1. Manual");
+            Console.WriteLine("2. Automatic");
+            var input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine("Enter 'S' at any time to see your current scores.");
+                newGame.ScoreBowl(true);
+            }
+            if (input == "2")
+            {
+                newGame.ScoreBowl(false);
+            }
             Console.ReadLine();
         }
     }
